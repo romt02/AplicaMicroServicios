dfffa51caf5c098f86350149749430a513729071
BGasser10 <53985983+BGasser10@users.noreply.github.com>
2019-09-29 15:16:04 -0500

    Revert "NO SOLUTION"
    This reverts commit 52d07451a1621b453d5eb0b3bc64ffd1f58f0453.

---
+++ axel/PW OS/apiFacebook.js
@@ -0,0 +1,4 @@
+<div id="fb" class="col-sm">
+<div class="fb-login-button" data-width="300" data-size="large" data-button-type="login_with"
+    data-auto-logout-link="true" data-use-continue-as="true"></div>
+</div>
--- axel/PW OS/api_E-Com.js
+++
@@ -1,5 +0,0 @@
-var myStore = new Commerce('pk_test_c57a31c6163575e6eebbe4ae4f33d8435d962f4f');
-//New Cart
-Commerce.Cart.init(function(resp){
-
-});
